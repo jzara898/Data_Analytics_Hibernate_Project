9139ee9ad4738fe149e12b5aa80869eb3686fae9
Jerri Zara <j2carter@ucsd.edu>
2025-01-07 15:17:38 -0800

    Update HibernateUtil.java

--- src/main/java/com/teamtreehouse/worldbank/util/HibernateUtil.java
+++ src/main/java/com/teamtreehouse/worldbank/util/HibernateUtil.java
@@ -5,7 +5,7 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
+//TODO:jcz
 public class HibernateUtil {
     private static StandardServiceRegistry registry;
     private static SessionFactory sessionFactory;
